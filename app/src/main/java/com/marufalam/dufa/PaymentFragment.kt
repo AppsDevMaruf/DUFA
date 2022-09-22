108b663d2714644c21f109165b5ecdb826cd2a63
maruf-alam <marufalam120@gmail.com>
2022-09-22 11:32:57 +0600

    signup

--- app/src/main/java/com/marufalam/dufa/PaymentFragment.kt
+++ app/src/main/java/com/marufalam/dufa/PaymentFragment.kt
@@ -1,59 +1,148 @@
 package com.marufalam.dufa
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import androidx.appcompat.widget.AppCompatSpinner
+import com.example.awesomedialog.*
+import com.sslwireless.sslcommerzlibrary.model.initializer.SSLCAdditionalInitializer
+import com.sslwireless.sslcommerzlibrary.model.initializer.SSLCommerzInitialization
+import com.sslwireless.sslcommerzlibrary.model.response.SSLCTransactionInfoModel
+import com.sslwireless.sslcommerzlibrary.model.util.SSLCCurrencyType
+import com.sslwireless.sslcommerzlibrary.model.util.SSLCSdkType
+import com.sslwireless.sslcommerzlibrary.view.singleton.IntegrateSSLCommerz
+import com.sslwireless.sslcommerzlibrary.viewmodel.listener.SSLCTransactionResponseListener
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PaymentFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class PaymentFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class PaymentFragment : Fragment(),SSLCTransactionResponseListener,AdapterView.OnItemSelectedListener{
+    lateinit var eTAmount: EditText
+    lateinit var showprice: TextView
+    lateinit var payButton: Button
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var sslCommerzInitialization: SSLCommerzInitialization? = null
+    var additionalInitializer: SSLCAdditionalInitializer? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_payment, container, false)
+        val view =inflater.inflate(R.layout.fragment_payment, container, false)
+        // get reference to the string array that we just created
+        val paymentType = resources.getStringArray(R.array.paymentsType)
+        // create an array adapter and pass the required parameter
+        // in our case pass the context, drop down layout , and array.
+        val arrayAdapter = ArrayAdapter(requireActivity(), R.layout.dropdown_item, paymentType)
+        // get reference to the autocomplete text view
+        val spinner = view.findViewById<AppCompatSpinner>(R.id.payment_spinner)
+        spinner.onItemSelectedListener = this
+        // set adapter to the autocomplete tv to the arrayAdapter
+        spinner.setAdapter(arrayAdapter)
+
+
+
+        payButton=view.findViewById(R.id.payNowButtonId)
+        showprice=view.findViewById(R.id.priceShow)
+
+
+        payButton.setOnClickListener {
+            val amount=showprice.text.toString().trim()
+            if (amount.isNotEmpty()){
+                sslSetUp(amount)
+            }else{
+                eTAmount.error="Error"
+            }
+        }
+        return view;
+    }
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, possition: Int, p3: Long) {
+        when(possition){
+            0->showprice!!.text = "1000"
+            1->showprice!!.text = "3000"
+            2->showprice!!.text = "5000"
+        }
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PaymentFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PaymentFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+
+    private fun sslSetUp(amount:String) {
+
+        val currentTimestamp = System.currentTimeMillis()
+
+        //ssl_commarz
+        sslCommerzInitialization = SSLCommerzInitialization(
+            "mobil5fe45035efe16",
+            "mobil5fe45035efe16@ssl",
+            amount.toDouble(), SSLCCurrencyType.BDT,
+            "MYID$currentTimestamp",
+            "yourProductType",
+            SSLCSdkType.TESTBOX
+        )
+
+        additionalInitializer = SSLCAdditionalInitializer()
+        additionalInitializer!!.valueA = "Value Option 1"
+        additionalInitializer!!.valueB = "Value Option 1"
+        additionalInitializer!!.valueC = "Value Option 1"
+        additionalInitializer!!.valueD = "Value Option 1"
+
+
+
+
+        IntegrateSSLCommerz
+            .getInstance(activity)
+            .addSSLCommerzInitialization(sslCommerzInitialization)
+            .addAdditionalInitializer(additionalInitializer)
+            .buildApiCall(this)
+
+
+    }
+    override fun transactionSuccess(successInfo: SSLCTransactionInfoModel?) {
+        if (successInfo != null) {
+
+
+            Log.d("BODYDATA", successInfo.tranId)
+            Log.d("BODYDATA", successInfo.status)
+
+            AwesomeDialog
+                .build(requireActivity())
+                .position(AwesomeDialog.POSITIONS.CENTER)
+                .title("Id:${successInfo.tranId} \nAmount: ${successInfo.amount} \nPayment Status:${successInfo.apiConnect}")
+                .icon(R.drawable.payment)
+                .onPositive("Continue") {
+
                 }
+
+        }
+    }
+
+    override fun transactionFail(p0: String?) {
+        AwesomeDialog
+            .build(requireActivity())
+            .position(AwesomeDialog.POSITIONS.CENTER)
+            .title("$p0")
+            .icon(R.mipmap.ic_launcher)
+            .onPositive("Continue") {
+
             }
     }
+
+    override fun merchantValidationError(p0: String?) {
+        AwesomeDialog
+            .build(requireActivity())
+            .position(AwesomeDialog.POSITIONS.CENTER)
+            .title("$p0")
+            .icon(R.mipmap.ic_launcher)
+            .onPositive("Continue") {
+
+            }
+    }
+
+
 }
---
+++ app/src/main/java/com/marufalam/dufa/fragments/signup/StrengthLevel.kt
@@ -0,0 +1,8 @@
+package com.example.passwordstrengthcalculator
+
+enum class StrengthLevel {
+    WEAK,
+    MEDIUM,
+    STRONG,
+    BULLETPROOF
+}
