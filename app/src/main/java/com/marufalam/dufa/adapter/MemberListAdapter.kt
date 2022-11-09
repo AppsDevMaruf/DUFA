cec3e80afeae0ce937a91c86971ec0f35d9aa92a
Maruf Alam <44240762+AppsDevMaruf@users.noreply.github.com>
2022-11-10 01:08:42 +0600

    refactor adapter error

--- app/src/main/java/com/marufalam/dufa/adapter/MemberListAdapter.kt
+++ app/src/main/java/com/marufalam/dufa/adapter/MemberListAdapter.kt
@@ -1,19 +1,95 @@
 package com.marufalam.dufa.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.android.material.imageview.ShapeableImageView
+import com.marufalam.dufa.R
+import com.marufalam.dufa.`interface`.MemberSelectListener
 import com.marufalam.dufa.databinding.MemberListRowBinding
 import com.marufalam.dufa.models.dashboard.ResponseMemberList
+class MemberListAdapter(
+    var memberSelectListener: MemberSelectListener,private var memberModelArrayList: ArrayList<ResponseMemberList>,
+    var context: Context
+) :
+    RecyclerView.Adapter<MemberListAdapter.ViewHolder>() {
 
-class MemberListAdapter :
-    ListAdapter<ResponseMemberList, MemberListAdapter.MemberListViewHolder>(MemberListDiffUtil()) {
+    // creating a variable for array list and context.
 
 
-    class MemberListViewHolder(val binding: MemberListRowBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+    // method for filtering our recyclerview items.
+    fun filterList(filterList: ArrayList<ResponseMemberList>) {
+        // below line is to add our filtered
+        // list in our course array list.
+        memberModelArrayList = filterList
+        // below line is to notify our adapter
+        // as change in recycler view data.
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // below line is to inflate our layout.
+        val view: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.member_list_row, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // setting data to our views of recycler view.
+        val model: ResponseMemberList =memberModelArrayList[position]
+        Glide.with(holder.img.context)
+            .load("")
+            .placeholder(R.drawable.loadpreview)
+            .into(holder.img)
+        holder.name.text = model.users
+
+
+        holder.itemView.setOnClickListener {
+            memberSelectListener.selectedMember(model)
+
+        }
+
+
+    }
+
+    override fun getItemCount(): Int {
+        // returning the size of array list.
+        return memberModelArrayList.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // creating variables for our views.
+        lateinit var name: TextView
+        var bloodGroup: TextView
+        var number: TextView
+        var img: ShapeableImageView
+
+        init {
+            // initializing our views with their ids.
+            name = itemView.findViewById(R.id.name)
+            bloodGroup = itemView.findViewById(R.id.blood_group)
+            number = itemView.findViewById(R.id.number)
+            img = itemView.findViewById(R.id.profile_image)
+
+        }
+    }
+
+    // creating a constructor for our variables.
+
+}
+
+
+
+    /*ListAdapter<ResponseMemberList, MemberListAdapter.MemberListViewHolder>(MemberListDiffUtil()) {
+
+
+    class MemberListViewHolder(val binding: MemberListRowBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: ResponseMemberList) {
             binding.user = item
         }
@@ -46,7 +122,5 @@ class MemberListAdapter :
             return oldItem == newItem
         }
 
-    }
+    }*/
 
-
-}
