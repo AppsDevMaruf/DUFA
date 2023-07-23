b7dd45d14c100b686baedaf0e63c99676c36ff47
MarufiMac <marufalam120@gmail.com>
2023-07-23 18:41:45 +0600

    add package name instrumented Test

--- app/src/androidTest/java/org/dufa/dufa9596/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/org/dufa/dufa9596/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.marufalam.dufa", appContext.packageName)
+        assertEquals("org.dufa.dufa9596", appContext.packageName)
     }
 }
